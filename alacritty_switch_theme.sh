6cf6c01c3dd2e73dd94402eacf8ed316e46a2063
Water Chika <water_chika@outlook.com>
2026-03-20 00:43:54 +0800

    add function alacritty_swith_theme; it need source to use it

--- alacritty_switch_theme.sh
+++ alacritty_switch_theme.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+function alacritty_switch_theme()
+{
 if test $1 == 'gray'; then
 alacritty msg config 'colors.primary.foreground="#000000"'
 alacritty msg config 'colors.primary.background="#ffffff"'
+unset COLORTERM
+export TERM=xtermm
 elif test $1 == 'eink'; then
 alacritty msg config 'colors.primary.foreground="#000000"'
 alacritty msg config 'colors.primary.background="#ffffff"'
@@ -10,3 +14,4 @@ elif test $1 == 'oled'; then
 alacritty msg config 'colors.primary.foreground="#a0a0a0"'
 alacritty msg config 'colors.primary.background="#000000"'
 fi
+}
