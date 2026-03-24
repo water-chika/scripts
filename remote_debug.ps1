f20636f6fcc40ffcb5707c2351594a3e87069055
Water Chika <water_chika@outlook.com>
2026-03-24 21:36:12 +0800

    update remote_debug

--- remote_debug.ps1
+++ remote_debug.ps1
@@ -1,10 +1,15 @@
 param (
     [Parameter(Mandatory)]
-    $RemoteHost
+    $RemoteHost,
+    [Parameter(Mandatory)]
+    $RemoteUser,
+    $DeployDir="C:/Debug",
+    [Parameter(Mandatory)]
+    $File
     )
 
 ninja -C build_ninja_release
-scp N:/scripts/debug_amd_driver.ps1 water@${RemoteHost}:C:/Debug
-scp build_ninja_release/atio6axx.dll water@${RemoteHost}:C:/Debug
-scp N:/scripts/remote_debug_remote.ps1 water@${RemoteHost}:C:/Debug
-ssh water@$RemoteHost "powershell C:/Debug/remote_debug_remote.ps1"
+scp $PSScriptRoot/debug_amd_driver.ps1 ${RemoteUser}@${RemoteHost}:${DeployDir}
+scp ${File} ${RemoteUser}@${RemoteHost}:${DeployDir}
+scp $PSScriptRoot/remote_debug_remote.ps1 ${RemoteUser}@${RemoteHost}:${DeployDir}
+ssh water@$RemoteHost "powershell ${DeployDir}/remote_debug_remote.ps1"
--- remote_debug_remote.ps1
+++ remote_debug_remote.ps1
@@ -1,4 +1,8 @@
-
+param (
+    $DeployDir="C:/Debug",
+    [Parameter(Mandatory)]
+    $File
+    )
 Set-Location C:/Debug/atio6axx
 Move-Item -Path 'atio6axx.dll' -Destination atio6axx-$(Get-Date -Format o | ForEach-Object{ $_ -replace ':',','}).dll
 Remove-Item -Confirm $false 'atio6axx-*.dll'
@@ -12,12 +16,6 @@ C:/Debug/debug_amd_driver.ps1 -ApiName "opengl"
 Write-Output "Done"
 
 Write-Output "Run test"
+// TODO:
+// Run test and store results
 
-Set-Location C:\SPEC\SPECgpc\SPECviewperf2020\
-
-$resolutions = @('1920x1080', '3840x2160')
-foreach ($resolution in $resolutions) {
-    New-Item -ItemType Directory "results"
-    Start-Process -Wait -FilePath .\viewperf\bin\viewperf.exe -ArgumentList '.\viewsets\medical\config\medical.xml','-resolution',$resolution
-    Move-Item -Path 'results' -Destination results-$(Get-Date -Format o | ForEach-Object{$_ -replace ':',','})
-}
