2a8fbcd35175a5fd7dde32a9f0c6b495b97d847c
Water Chika <water_chika@outlook.com>
2026-04-04 23:55:36 +0800

    fix typo

--- add_vulkan_layer.ps1
+++ add_vulkan_layer.ps1
@@ -1,11 +1,11 @@
 param(
     $JsonPath,
-    $ImplitLayer=$true
+    $ImplicitLayer=$true
 )
 
 $reg_path = "HKLM:\SOFTWARE\Khronos\Vulkan\ExplicitLayers"
 if ($ImplitLayer) {
-    $reg_path = "HKLM:\SOFTWARE\Khronos\Vulkan\ImplitLayers"
+    $reg_path = "HKLM:\SOFTWARE\Khronos\Vulkan\ImplicitLayers"
 }
 Set-ItemProperty -Path $reg_path -Name $JsonPath -Value 0
 
