c31b4c176bdf2dca04373dc051c764eb384b4999
Water Chika <water_chika@outlook.com>
2026-04-19 18:55:33 +0800

    update alacritty_switch_theme.sh

--- alacritty_switch_theme.sh
+++ alacritty_switch_theme.sh
@@ -18,6 +18,11 @@ alacritty msg config 'colors.primary.background="#000000"'
 fi
 }
 
+function get_focused_output_make()
+{
+	swaymsg -t get_outputs | jq -r '.. | select(.focused?) | .make'
+}
+
 function alacritty_get_focused_output_name()
 {
 	swaymsg -t get_outputs | jq -r '.. | select(.focused?) | .make + " " + .model'
@@ -25,11 +30,14 @@ function alacritty_get_focused_output_name()
 
 if test $TERM == 'alacritty'; then
 	export output_name=$(alacritty_get_focused_output_name)
+	export output_make=$(get_focused_output_make)
 	if test "$output_name" == 'DSC Paperlike H D'; then
 		alacritty_switch_theme gray
 	elif test "$output_name" == 'Invalid Vendor Codename - RTK DS-DP'; then
 		alacritty_switch_theme eink
-	elif test "$output_name" == 'Samsung Electric Company Odyssey G81SF'; then
+	elif test "$output_make" == 'Samsung Electric Company'; then
 		alacritty_switch_theme oled
+	else
+		alacritty_switch_theme eink
 	fi
 fi
