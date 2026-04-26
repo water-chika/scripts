018f656acebd3819261681e246b897b7e938e20f
Water Chika <water_chika@outlook.com>
2026-04-26 12:52:05 +0800

    fix typo

--- tablet_as_screen.sh
+++ tablet_as_screen.sh
@@ -3,5 +3,5 @@
 swaymsg create-output
 swaymsg output HEADLESS-1 mode 2480x1860 allow_tearing yes
 
-swayvnc --output=HEADLESS-1 --disable-input --render-cursor 127.0.0.1 &
+wayvnc --output=HEADLESS-1 --disable-input --render-cursor 127.0.0.1 &
 ssh tablet.water.n2n -R 127.0.0.1:5900:127.0.0.1:5900 -N
