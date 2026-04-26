53c388311a0b2b5cbfcafe1e562d581bb34ba889
Water Chika <water_chika@outlook.com>
2026-04-26 13:11:15 +0800

    tablet_as_screen.sh: detect if virtual output exist

--- tablet_as_screen.sh
+++ tablet_as_screen.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 
-swaymsg create-output
-swaymsg output HEADLESS-1 mode 2480x1860 allow_tearing yes
+name=$(swaymsg -t get_outputs | jq -r '.[] | select(.name=="HEADLESS-1") | .name')
+
+if test $name != "HEADLESS-1"; then
+	swaymsg create_output
+	swaymsg output HEADLESS-1 mode 2480x1860 allow_tearing yes scale 2
+fi
 
 wayvnc --output=HEADLESS-1 --disable-input --render-cursor 127.0.0.1 &
 ssh tablet.water.n2n -R 127.0.0.1:5900:127.0.0.1:5900 -N
