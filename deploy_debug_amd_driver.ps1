df5385613992dd22e7391bdf9c985b04e209063f
Water Chika <water_chika@outlook.com>
2026-06-03 15:02:01 +0800

    deploy_debug_amd_driver.ps1: add parameter DeployDirectory

--- deploy_debug_amd_driver.ps1
+++ deploy_debug_amd_driver.ps1
@@ -4,14 +4,18 @@ param (
     [Parameter(Mandatory)]
     $DebugDriverPath,
     [Parameter(Mandatory)]
-    $Api
+    $Api,
+    $DeployDirectory="C:/workspace/debug"
     )
 
-scp $PSScriptRoot/debug_amd_driver.ps1 water@${RemoteHost}:C:/Debug
-$driver_name = Split-Path $DebugDriverPath -Leaf
-scp $DebugDriverPath water@${RemoteHost}:C:/Debug
+// Make sure deploy directory exists
+ssh water@${RemoteHost} mkdir $DeployDirectory
 
-Write-Output "powershell C:/Debug/debug_amd_driver.ps1 -Api $Api -DebugDriver C:/Debug/$driver_name -Restore $true"
-ssh water@$RemoteHost "powershell C:/Debug/debug_amd_driver.ps1 -Api $Api -DebugDriver C:/Debug/$driver_name -Restore $true"
-Write-Output "powershell C:/Debug/debug_amd_driver.ps1 -Api $Api -DebugDriver C:/Debug/$driver_name"
-ssh water@$RemoteHost "powershell C:/Debug/debug_amd_driver.ps1 -Api $Api -DebugDriver C:/Debug/$driver_name"
+scp $PSScriptRoot/debug_amd_driver.ps1 water@${RemoteHost}:${DeployDirectory}
+$driver_name = Split-Path $DebugDriverPath -Leaf
+scp $DebugDriverPath water@${RemoteHost}:${DeployDirectory}
+
+Write-Output "powershell ${DeployDirectory}/debug_amd_driver.ps1 -Api $Api -DebugDriver ${DeployDirectory}/$driver_name -Restore $true"
+ssh water@$RemoteHost "powershell ${DeployDirectory}/debug_amd_driver.ps1 -Api $Api -DebugDriver ${DeployDirectory}/$driver_name -Restore $true"
+Write-Output "powershell ${DeployDirectory}/debug_amd_driver.ps1 -Api $Api -DebugDriver ${DeployDirectory}/$driver_name"
+ssh water@$RemoteHost "powershell ${DeployDirectory}/debug_amd_driver.ps1 -Api $Api -DebugDriver ${DeployDirectory}/$driver_name"
